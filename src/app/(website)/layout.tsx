bac190425aadc3f3cce093d729fa416d1c1078b9
Bright Munemu <brightmunemu@gmail.com>
2025-11-06 22:35:27 +0100

    add style

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -7,7 +7,7 @@ export default function WebSiteLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="bg-[radial-gradient(circle_at_top,#FAFAFA_6%,#E4EAFB_8%)] sm:bg-[radial-gradient(circle_at_top,#FAFAFA_5%,#E4EAFB_15%)] lg:bg-[radial-gradient(circle_at_top,#FAFAFA_12%,#E4EAFB_15%)] 2xl:bg-[radial-gradient(circle_at_top,#FAFAFA_12%,#E4EAFB_50%)] overflow-hidden">
+    <main className="bg-[#FFF8EB]">
       <Header />
       {children}
       <Footer />
--- src/components/web-site/_shared/header/index.tsx
+++ src/components/web-site/_shared/header/index.tsx
@@ -9,10 +9,12 @@ export default function Header() {
   const pathName = usePathname();
 
   return (
-    <header className="container mx-auto py-5 px-5 z-20 sticky top-0 left-0 w-full">
+    <header className="container mx-auto py-5 px-5 z-20 sticky top-0 left-0 w-full bg-[#FFF8EB]">
       <div className="grid grid-flow-col justify-between lg:grid-cols-[auto_auto] items-center gap-8 lg:gap-16">
         {/* Logo */}
-        <Logo className="w-32" />
+        <Link href="/" target="_blank" className="grid whitespace-nowrap">
+          <Logo className="w-32" />
+        </Link>
 
         {/* Navigation  */}
         <Navigation />
