37390f7a7be0030056df18a5b6551b0a1db27fc0
Bright Munemu <brightmunemu@gmail.com>
2025-11-08 00:44:22 +0100

    create btn

--- src/ui/button/index.tsx
+++ src/ui/button/index.tsx
@@ -4,11 +4,15 @@ import Default from "./default";
 import CreamFilledRounded from "./cream-filled-rounded";
 import YellowFilledRounded from "./yellow-filled-rounded";
 import SkyBlueFilledRounded from "./sky-blue-filled-rounded";
+import GreenFilledRounded from "./green-filled-rounded";
 
 export function Button(props: ButtonProps) {
   switch (props.variant) {
     case ButtonVariants.CreamFilledRounded:
       return <CreamFilledRounded {...props} />;
+
+    case ButtonVariants.GreenFilledRounded:
+      return <GreenFilledRounded {...props} />;
 
     case ButtonVariants.YellowFilledRounded:
       return <YellowFilledRounded {...props} />;
--- src/ui/button/types.ts
+++ src/ui/button/types.ts
@@ -11,6 +11,7 @@ export interface ButtonProps {
 export enum ButtonVariants {
   CreamFilledRounded = "cream-filled-rounded",
   Default = "default",
+  GreenFilledRounded = "green-filled-rounded",
   YellowFilledRounded = "sky-blue-filled-rounded",
   SkyBlueFilledRounded = "sky-blue-filled-rounded",
 }
