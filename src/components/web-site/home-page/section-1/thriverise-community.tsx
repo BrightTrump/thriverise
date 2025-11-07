73ca89bbdc2513ad6736185f9c39baf4b5dfd10c
Bright Munemu <brightmunemu@gmail.com>
2025-11-08 00:36:24 +0100

    update

--- src/components/web-site/home-page/section-1/thriverise-community.tsx
+++ src/components/web-site/home-page/section-1/thriverise-community.tsx
@@ -4,8 +4,8 @@ import { Button, ButtonVariants } from "@/ui";
 
 export default function ThriveriseCommunity() {
   return (
-    <div className="grid lg:grid-cols-2 gap-3 lg:gap-24 items-center  text-[#183668]">
-      <span className="relative w-full aspect-[1/0.7]">
+    <div className="grid lg:grid-cols-2 gap-3 lg:gap-24 items-center text-[#183668]">
+      <span className="relative w-full aspect-[1/0.7] ">
         <Image
           src={"/website-ui/latest-projects.svg"}
           fill
@@ -14,7 +14,7 @@ export default function ThriveriseCommunity() {
         />
       </span>
 
-      <div className="grid gap-5 lg:gap-8 text-[#183668]">
+      <div className="grid gap-5 lg:gap-8 text-[#183668] pl-5">
         <div className="grid gap-2.5">
           <h1 className="uppercase font-bold text-2xl md:text-4xl">
             ThriveRise Community Learning Centers
--- src/components/web-site/home-page/section-1/thriverise-library.tsx
+++ src/components/web-site/home-page/section-1/thriverise-library.tsx
@@ -4,8 +4,8 @@ import { Button, ButtonVariants } from "@/ui";
 
 export default function ThriveriseLibrary() {
   return (
-    <div className="grid lg:grid-cols-2 gap-3 lg:gap-24 items-center  text-[#183668]">
-      <div className="grid gap-5 lg:gap-8 text-[#183668] order-1 lg:order-none">
+    <div className="grid lg:grid-cols-2 gap-3 lg:gap-24 items-center text-[#183668]">
+      <div className="grid gap-5 lg:gap-8 text-[#183668] order-2 lg:order-none pl-5">
         <div className="grid gap-2.5">
           <h1 className="uppercase font-bold text-2xl md:text-4xl">
             ThriveRise Library
@@ -21,7 +21,7 @@ export default function ThriveriseLibrary() {
           </Button>
         </div>
       </div>
-      <span className="relative w-full aspect-[1/0.7] order-2 lg:order-none">
+      <span className="relative w-full aspect-[1/0.7] mx-auto order-1 lg:order-none">
         <Image
           src={"/website-ui/thriverise-library.svg"}
           fill
