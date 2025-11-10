2a148559d4833bca18f2d7e8eb939f91b74dd915
Bright Munemu <brightmunemu@gmail.com>
2025-11-10 17:33:02 +0100

    add

--- src/components/web-site/home-page/section-4/index.tsx
+++ src/components/web-site/home-page/section-4/index.tsx
@@ -14,28 +14,28 @@ export default function Section4() {
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 2, ease: "easeOut" }}
           viewport={{ once: true, amount: 0.2 }}
-          className="grid lg:grid-cols-2 gap-3 lg:gap-24 items-center text-[#183668]"
+          className="grid lg:grid-cols-2 gap-3 lg:gap-24 items-center bg-[#FDB813] text-[#183668]"
         >
           <div className="grid gap-5 lg:gap-8 text-[#183668] order-2 lg:order-none pl-5">
             <div className="grid gap-2.5">
               <h1 className="uppercase font-bold text-2xl md:text-4xl">
-                ThriveRise Library
+                Become a Campus Ambassador
               </h1>
               <p className="lg:max-w-sm 2xl:max-w-xl">
-                A digital library offering free access to academic resources,
-                research papers, and tools for university students across
-                Nigeria.
+                Are you passionate about education and student access? Join the
+                ThriveRise Campus Ambassadors Program and become a voice for
+                change in your university.
               </p>
             </div>
             <div>
               <Button variant={ButtonVariants.SkyBlueFilledRounded}>
-                Request Resources
+                Join as a Campus Ambassador
               </Button>
             </div>
           </div>
           <span className="relative w-full aspect-[1/0.7] mx-auto order-1 lg:order-none">
             <Image
-              src={"/website-ui/thriverise-library.svg"}
+              src={"/website-ui/campus-ambassador.svg"}
               fill
               sizes="1024px"
               alt="Icon"
