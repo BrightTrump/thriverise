af3171fbe7f5ec41a686181fde5bd0b7e09b754e
Bright Munemu <brightmunemu@gmail.com>
2025-11-25 10:23:53 +0100

    sun

---
+++ src/app/(website)/resources/page.tsx
@@ -0,0 +1,9 @@
+import SectionResources1 from "@/components/web-site/resources/section-res-1";
+
+export default function Page() {
+  return (
+    <main className="overflow-hidden">
+      <SectionResources1 />
+    </main>
+  );
+}
