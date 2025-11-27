12a773f56529ec7fa6cc6a339e0874cecea30173
Bright Munemu <brightmunemu@gmail.com>
2025-11-27 21:47:36 +0100

    add

--- src/components/web-site/resources/section-res-3/annual-highlights.tsx
+++ src/components/web-site/resources/section-res-3/annual-highlights.tsx
@@ -1,34 +1,34 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-export default function WeBelieve() {
+export default function AnnualHighlights() {
   return (
     <motion.div
       initial={{ opacity: 0, x: 100 }}
       whileInView={{ opacity: 1, x: 0 }}
       transition={{ duration: 2, ease: "easeOut" }}
       viewport={{ once: true, amount: 0.2 }}
-      className="grid gap-8 text-[#FFF8EB]"
+      className="grid gap-7 md:gap-12 text-[#FFF8EB]"
     >
       <div className="text-center">
         <h1 className="uppercase font-bold text-center text-4xl lg:text-6xl">
-          We Believe
+          Annual Highlights
         </h1>
       </div>
 
       {/* Content */}
-      <div className="font-bold md:font-normal grid md:grid-flow-col gap-3 [&_span]:font-bold [&_span]:text-[80px] sm:[&_span]:text-8xl [&_span]:text-[#ffffff54] [&_p]:text-3xl md:[&_p]:text-4xl">
+      <div className="font-bold grid md:grid-flow-col gap-3 [&_span]:text-[80px] sm:[&_span]:text-8xl [&_span]:text-[#ffffff54] [&_p]:text-3xl md:[&_p]:text-4xl">
         <div>
           <span>01</span>
-          <p>Everyone deserves a chance to learn.</p>
+          <p>2025 End of Year Impact Report</p>
         </div>
         <div>
           <span>02</span>
-          <p>Communities can rise when empowered.</p>
+          <p>2025 ThriveRise Library Report</p>
         </div>
         <div>
           <span>03</span>
-          <p>Small efforts lead to big change.</p>
+          <p>Volunteer & Donor Engagement Summary</p>
         </div>
       </div>
     </motion.div>
--- src/components/web-site/resources/section-res-3/index.tsx
+++ src/components/web-site/resources/section-res-3/index.tsx
@@ -1,13 +1,23 @@
 import React from "react";
-import WhoWeAre from "./who-we-are";
-import WeBelieve from "./we-believe";
+import AnnualHighlights from "./annual-highlights";
 
 export default function SectionResources3() {
   return (
-    <section className="py-10 sm:py-20 layout-spacing bg-[#279B48]">
-      <div className="container mx-auto grid gap-14 lg:gap-48">
-        <WhoWeAre />
-        <WeBelieve />
+    <section className="py-10 sm:py-20 layout-spacing bg-[#183668]">
+      <div className="container mx-auto grid gap-12 lg:gap-16 items-center place-items-center">
+        <div className="max-w-3xl text-[#183668] text-center">
+          <h1 className="uppercase font-bold text-center text-4xl md:text-6xl">
+            Stories That Matter
+          </h1>
+          <p>
+            Dive into inspiring stories from our volunteers, educators,
+            community members, and students. Our blog highlights the people and
+            purpose behind everything we do.
+          </p>
+        </div>
+        <div>
+          <AnnualHighlights />
+        </div>
       </div>
     </section>
   );
