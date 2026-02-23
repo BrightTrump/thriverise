e01cefd14074458303b862127b0d7a698af02eb5
Bright Munemu <brightmunemu@gmail.com>
2026-02-23 08:38:32 +0100

    add

--- src/components/web-site/campus-ambassador/campus-section-6/index.tsx
+++ src/components/web-site/campus-ambassador/campus-section-6/index.tsx
@@ -9,36 +9,24 @@ export default function CampusSection6() {
       <div className="container mx-auto max-w-6xl px-3 sm:px-6 md:px-12 grid gap-8 bg-[#183668] rounded-4xl py-16">
         <div className="grid gap-2.5 text-white [&>p]:max-w-sm [&>p]:md:max-w-lg [&>p]:lg:max-w-xl [&>p]:md:text-lg">
           <h1 className="uppercase font-bold text-4xl md:text-6xl">
-            Get Involved
+            Ready to Lead Change?
           </h1>
           <p>
-            Whether you&apos;re a student, educator, or professional, you can
-            contribute by donating academic resources, volunteering your
-            expertise, or spreading the word.
+            If you&apos;re ready to inspire and empower your campus community,
+            we’d love to have you join us!
           </p>
         </div>
-        <div className="grid sm:grid-cols-[auto_1fr] gap-5 sm:gap-2 items-center">
-          <Link
-            href={
-              "https://docs.google.com/forms/d/e/1FAIpQLSePx3k6-oCZ2uo6w9r1oh2zxq9sD_oWBXq_4hzxlkHgRfeqeg/viewform"
-            }
-            target="_blank"
-          >
-            <Button variant={ButtonVariants.SkyBlueFilledRounded}>
-              Request Resources
-            </Button>
-          </Link>
-          <Link
-            href={
-              "https://docs.google.com/forms/d/1UBYb_oTB21N8etqDTqS1CbWJx5gpBnQcUMSELpzvsuw/viewform"
-            }
-            target="_blank"
-          >
-            <Button variant={ButtonVariants.SkyBlueFilledRounded}>
-              Donate Materials
-            </Button>
-          </Link>
-        </div>
+
+        <Link
+          href={
+            "https://docs.google.com/forms/d/e/1FAIpQLSePx3k6-oCZ2uo6w9r1oh2zxq9sD_oWBXq_4hzxlkHgRfeqeg/viewform"
+          }
+          target="_blank"
+        >
+          <Button variant={ButtonVariants.SkyBlueFilledRounded}>
+            Become a Campus Ambassador
+          </Button>
+        </Link>
       </div>
     </section>
   );
