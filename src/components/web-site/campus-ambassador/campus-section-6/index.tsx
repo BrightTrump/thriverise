baf5117f99e2ed28d572a724d74038fa0eba4f20
Bright Munemu <brightmunemu@gmail.com>
2026-02-23 09:02:32 +0100

    add

--- src/components/web-site/campus-ambassador/campus-section-6/index.tsx
+++ src/components/web-site/campus-ambassador/campus-section-6/index.tsx
@@ -9,7 +9,7 @@ export default function CampusSection6() {
       <div className="container mx-auto max-w-6xl px-3 sm:px-6 md:px-12 grid gap-8 bg-[#183668] rounded-4xl py-16">
         <div className="grid gap-2.5 text-white [&>p]:max-w-sm [&>p]:md:max-w-lg [&>p]:lg:max-w-xl [&>p]:md:text-lg">
           <h1 className="uppercase font-bold text-4xl md:text-6xl">
-            Ready to Lead Change?
+            Ready to Lead Change????
           </h1>
           <p>
             If you&apos;re ready to inspire and empower your campus community,
@@ -17,12 +17,7 @@ export default function CampusSection6() {
           </p>
         </div>
 
-        <Link
-          href={
-            ""
-          }
-          target="_blank"
-        >
+        <Link href={""} target="_blank">
           <Button variant={ButtonVariants.SkyBlueFilledRounded}>
             Become a Campus Ambassador
           </Button>
