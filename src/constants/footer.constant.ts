2ce1caf15cf0831eb9dcafec6dee0905e2283db9
Bright Munemu <brightmunemu@gmail.com>
2026-03-09 22:34:04 +0100

    ad url

--- src/constants/footer.constant.ts
+++ src/constants/footer.constant.ts
@@ -1,8 +1,14 @@
 import { Icons } from "@/ui";
 
 export const SOCIALS_SCHEMA = [
-  { icon: Icons.Instagram, url: "" },
+  {
+    icon: Icons.Instagram,
+    url: "https://www.instagram.com/thriverise_foundation?igsh=MTc5eG1yajdyb2R1dg%3D%3D&utm_source=qr",
+  },
   { icon: Icons.Twitter, url: "https://x.com/thriverise_?s=11" },
-  { icon: Icons.Facebook, url: "" },
+  {
+    icon: Icons.Facebook,
+    url: "https://www.facebook.com/share/1AD1ihnUzd/?mibextid=wwXIfr",
+  },
   { icon: Icons.Linkedin, url: "https://www.linkedin.com/company/thriverise/" },
 ];
