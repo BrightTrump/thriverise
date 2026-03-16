8915c66518ceccbe64e070a9dc1e3a23089edc7c
Bright Munemu <brightmunemu@gmail.com>
2026-03-16 10:46:18 +0100

    add

--- src/app/(website)/contact/page.tsx
+++ src/app/(website)/contact/page.tsx
@@ -1,9 +1,11 @@
 import SectionContact1 from "@/components/web-site/contact/section-cont-1";
+import SectionContact2 from "@/components/web-site/contact/section-cont-2";
 
 export default function Page() {
   return (
     <main className="overflow-hidden">
       <SectionContact1 />
+      <SectionContact2 />
     </main>
   );
 }
--- src/components/web-site/contact/section-cont-2/index.tsx
+++ src/components/web-site/contact/section-cont-2/index.tsx
@@ -10,14 +10,16 @@ import {
 export default function SectionContact2() {
   return (
     <section className="py-10 sm:py-20 layout-spacing">
-      <Accordion type="single" defaultValue="item-1" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+      <div className="container mx-auto max-w-4xl">
+        <Accordion type="single" defaultValue="item-1" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </section>
   );
 }
