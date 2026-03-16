7d1343a7c422629013ad8fc91a1cf07d4fa52e54
Bright Munemu <brightmunemu@gmail.com>
2026-03-16 11:19:26 +0100

    add link

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -13,7 +13,10 @@ function Accordion({
   return (
     <AccordionPrimitive.Root
       data-slot="accordion"
-      className={cn("flex w-full flex-col", className)}
+      className={cn(
+        "flex w-full flex-col justify-b text-[#183668] bg-white rounded-xl border-2 border-[#01AED9]",
+        className,
+      )}
       {...props}
     />
   );
@@ -42,7 +45,7 @@ function AccordionTrigger({
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
         className={cn(
-          "group/accordion-trigger relative flex flex-1 items-start justify-between rounded-lg border border-transparent py-2.5 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 focus-visible:after:border-ring disabled:pointer-events-none disabled:opacity-50 **:data-[slot=accordion-trigger-icon]:ml-auto **:data-[slot=accordion-trigger-icon]:size-4 **:data-[slot=accordion-trigger-icon]:text-muted-foreground",
+          "group/accordion-trigger relative flex flex-1 items-start justify-between rounded-lg  py-2.5 text-left text-[22px] font-bold transition-all outline-none hover:underline focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 focus-visible:after:border-ring disabled:pointer-events-none disabled:opacity-50 **:data-[slot=accordion-trigger-icon]:ml-auto **:data-[slot=accordion-trigger-icon]:size-4 **:data-[slot=accordion-trigger-icon]:text-muted-foreground border-2 border-[#01AED9] px-2",
           className,
         )}
         {...props}
@@ -69,12 +72,12 @@ function AccordionContent({
   return (
     <AccordionPrimitive.Content
       data-slot="accordion-content"
-      className="overflow-hidden text-sm data-open:animate-accordion-down data-closed:animate-accordion-up"
+      className="overflow-hidden text-sm data-[state=open]:animate-accordion-down data-[state=closed]:animate-accordion-up"
       {...props}
     >
       <div
         className={cn(
-          "h-(--radix-accordion-content-height) pt-0 pb-2.5 [&_a]:underline [&_a]:underline-offset-3 [&_a]:hover:text-foreground [&_p:not(:last-child)]:mb-4",
+          "pt-0 pb-2.5 p-2 [&_a]:underline [&_a]:underline-offset-3 [&_a]:hover:text-foreground [&_p:not(:last-child)]:mb-4",
           className,
         )}
       >
--- src/components/web-site/contact/section-cont-2/index.tsx
+++ src/components/web-site/contact/section-cont-2/index.tsx
@@ -9,13 +9,26 @@ import {
 
 export default function SectionContact2() {
   return (
-    <section className="py-10 sm:py-20 layout-spacing">
-      <div className="container mx-auto max-w-4xl">
+    <section className="py-10 sm:py-20 layout-spacing bg-[#183668]">
+      <div className="container mx-auto max-w-4xl grid gap-4">
         <Accordion type="single" defaultValue="item-1" collapsible>
           <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionTrigger>
+              Can I donate books or learning materials?
+            </AccordionTrigger>
             <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
+              Yes! Reach out via email or DM, and we&apos;ll guide you on what’s
+              most needed.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <Accordion type="single" defaultValue="item-1" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Where can I volunteer?</AccordionTrigger>
+            <AccordionContent>
+              You can volunteer virtually or in person (currently in Kwara
+              State)or <br /> Visit our{" "}
+              <link href={"/get-involved"}>Get Involved</link> page to sign up.
             </AccordionContent>
           </AccordionItem>
         </Accordion>
