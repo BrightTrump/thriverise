21d159ecddb8b4ef7eeee5ef37a878083fad35c2
Bright Munemu <brightmunemu@gmail.com>
2026-03-16 10:42:50 +0100

    add

---
+++ src/components/web-site/contact/section-cont-2/index.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+export default function SectionContact2() {
+  return (
+    <section className="py-10 sm:py-20 layout-spacing">
+      <Accordion type="single" defaultValue="item-1" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </section>
+  );
+}
